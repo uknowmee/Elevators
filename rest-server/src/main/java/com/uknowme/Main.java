4576c68d39f1fb166cfa06753b404e0b9eeb10e5
uknowmee <michalpalucki@icloud.com>
2023-05-10 11:40:25 +0200

    init rest server

---
+++ rest-server/src/main/java/com/uknowme/Main.java
@@ -0,0 +1,12 @@
+package com.uknowme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//http://localhost:8080/swagger-ui
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
---
+++ rest-server/src/main/java/com/uknowme/controller/Smoke.java
@@ -0,0 +1,22 @@
+package com.uknowme.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("api/v1/smoke")
+@AllArgsConstructor
+public class Smoke {
+
+    private record SmokeResponse(String message) {
+    }
+
+    @GetMapping
+    public SmokeResponse getSmoke() {
+        return new SmokeResponse("Smoke test passed!");
+    }
+}
