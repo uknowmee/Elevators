1301018df812602a952290a9be3e77b4dc1df941
uknowmee <michalpalucki@icloud.com>
2023-05-12 22:35:41 +0200

    .

--- rest-server/src/main/java/com/uknowme/services/floor/FloorServiceImpl.java
+++ rest-server/src/main/java/com/uknowme/services/floor/FloorServiceImpl.java
@@ -1,7 +1,6 @@
 package com.uknowme.services.floor;
 
 import com.uknowme.domain.Floor;
-import com.uknowme.repositories.FloorRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
